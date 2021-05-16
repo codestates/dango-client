732381d72563afdaa9d67b50963d495b818d3725
YU-WON <shallwedance0419@gmail.com>
2021-05-16 16:19:14 +0900

    [JAWS-14][Add] imageUploader

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,7 +1,23 @@
 import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
-function App() {
-  return <div className="App">Yummy Dango!</div>;
+import TalentRegistrationPage from './pages/TalentRegistrationPage/TalentRegistrationPage';
+
+function App():JSX.Element {
+  return (
+    <>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/">
+          <div className="App">Yummy Dango!</div>
+        </Route>
+        <Route exact path="/registration">
+          <TalentRegistrationPage />
+        </Route>
+      </Switch>
+    </BrowserRouter>
+    </>
+  )
 }
 
 export default App;
