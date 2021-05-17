db19c4315eef557fa41f26fe14b7cde5a670266c
SoonKi Kwon <jhk327@gmail.com>
2021-05-17 16:26:56 +0900

    [JAWS-27][Add] page setting

---
+++ src/components/pages/TalentRegistrationPage/Sections/LocationSearch.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const CONTAINER = styled.div`
+  grid-column: 1/10;
+  grid-row: 3/13;
+  border: 1px solid black;
+`;
+
+function LocationSearch(): JSX.Element {
+  return <CONTAINER>ddd</CONTAINER>;
+}
+
+export default LocationSearch;
--- src/components/pages/TalentRegistrationPage/TalentRegistrationPage.tsx
+++ src/components/pages/TalentRegistrationPage/TalentRegistrationPage.tsx
@@ -1,8 +1,19 @@
 import React from 'react';
+import styled from 'styled-components';
 import ImageUploadr from './Sections/imageUploader';
+import LocationSearch from './Sections/LocationSearch';
 
-export default function TalentRegistrationPage():JSX.Element {
-    return (
-        <ImageUploadr />
-    )
-};
+const CONTAINER = styled.div`
+  display: grid;
+  height: 100vh;
+  gap: 20px;
+`;
+
+export default function TalentRegistrationPage(): JSX.Element {
+  return (
+    <CONTAINER>
+      <ImageUploadr />
+      <LocationSearch />
+    </CONTAINER>
+  );
+}
