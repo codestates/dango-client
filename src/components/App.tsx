86415d1cea231cd50dfa122550a3d0696c89a97e
SoonKi Kwon <jhk327@gmail.com>
2021-05-18 20:10:25 +0900

    [JAWS-27][Add] set basic

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import 'dotenv/config';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import GlobalStyles from './GlobalStyles';
 import Kakao from './pages/SigninPage/kakao';
--- src/components/pages/TalentRegistrationPage/TalentRegistrationPage.tsx
+++ src/components/pages/TalentRegistrationPage/TalentRegistrationPage.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 import ImageUploadr from './Sections/imageUploader';
 import LocationSearch from './Sections/LocationSearch';
@@ -9,11 +9,29 @@ const CONTAINER = styled.div`
   gap: 20px;
 `;
 
+const FORM = styled.form`
+  display: grid;
+  justify-items: center;
+  align-items: start;
+  grid-column: 1/10;
+  grid-row: 3/13;
+  border: 1px solid black;
+`;
+
 export default function TalentRegistrationPage(): JSX.Element {
+  const [location, setLocation] = useState<number[]>([]);
+  const [address, setAddress] = useState<string>();
+
+  useEffect(() => {
+    console.log('location::::', location);
+    console.log('address::::', address);
+  }, [location, address]);
   return (
     <CONTAINER>
+      <FORM>
+        <LocationSearch setLocation={setLocation} setAddress={setAddress} />
+      </FORM>
       <ImageUploadr />
-      <LocationSearch />
     </CONTAINER>
   );
 }
