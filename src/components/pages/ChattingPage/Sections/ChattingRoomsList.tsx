016bcf9bf42e655a4cfeb609bee76ff9e1e4ce6c
Jebbit-koi <75734684+Jebbit-koi@users.noreply.github.com>
2021-05-23 23:00:31 +0900

    [JAWS-49][Add] chattingRoomsList

--- src/components/pages/ChattingPage/Sections/ChattingRoomsList.tsx
+++ src/components/pages/ChattingPage/Sections/ChattingRoomsList.tsx
@@ -1,7 +1,21 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import { RootState } from '../../../../_reducer';
 
-function ChattingRoomsList() {
-  return <div>list</div>;
+function ChattingRoomsList(): JSX.Element {
+  const { userInfo } = useSelector((state: RootState) => state.user);
+
+  console.log('채팅방 목록: ', userInfo?.chatRooms);
+
+  return (
+    <div>
+      {userInfo?.chatRooms?.map((chatRoom) => (
+        <div key={chatRoom.roomId}>
+          {chatRoom.other}안 읽음 메시지{chatRoom.count}
+        </div>
+      ))}
+    </div>
+  );
 }
 
 export default ChattingRoomsList;
