1f71522fe7b918cae78ac30074ad260ea9c04ef3
Jebbit-koi <75734684+Jebbit-koi@users.noreply.github.com>
2021-05-24 22:39:40 +0900

    [JAWS-55][Add] kakao link

--- src/components/pages/TalentDetailPage/TalentDetailPage.tsx
+++ src/components/pages/TalentDetailPage/TalentDetailPage.tsx
@@ -3,6 +3,7 @@ import { useParams } from 'react-router-dom';
 import Review from './Sections/Review';
 import server from '../../../api';
 import { CONTAINER, SELLER, DETAIL, PHOTOS } from './TalentDetailPageStyle';
+import dangoImage from '../../../images/dangoImage.jpeg';
 
 // 여기서 해당 글의 정보를 서버에서 받고, 리덕스에 저장한다.
 // 서버요청의 useEffect의 deps배열안에는 변할수 있는 상태를 넣어준다.
@@ -16,8 +17,15 @@ import { CONTAINER, SELLER, DETAIL, PHOTOS } from './TalentDetailPageStyle';
 // TODO: 구매자인경우 userReducer에서 구매내역의 talentId의 수와
 //       talet의 review를 작성한 내userId의 개수를 비교해야함
 //       내구매내역의 talentId의 갯수(또는 talent의 buyer안의 내 userId 갯수) > 리뷰작성한 내 userId의 갯수이면 리뷰작성창 나와야한다.
+declare global {
+  interface Window {
+    Kakao: any;
+  }
+}
 
 function TalentDetailPage(): JSX.Element {
+  const { Kakao } = window;
+
   const [detailData, setDetailData] = useState<any>();
   const [editDetail, setEditDetail] = useState<any>();
   const [isClicked, setIsClicked] = useState<boolean>(false);
@@ -34,32 +42,64 @@ function TalentDetailPage(): JSX.Element {
     });
   }, []);
 
+  useEffect(() => {
+    Kakao.Link.createDefaultButton({
+      container: '.create-kakao-link-btn', // 버튼 class name
+      objectType: 'location',
+      address: detailData ? detailData.address : '경기 성남시 분당구 판교역로 235 에이치스퀘어 N동 8층', // required라서 없으면 에러 남.
+      addressTitle: 'DANGO', // 지도에서 표시될 이름
+      content: {
+        title: `DANGO와 나누는 재능, ${detailData?.title}`,
+        description: detailData?.description,
+        imageUrl: 'http://k.kakaocdn.net/dn/bSbH9w/btqgegaEDfW/vD9KKV0hEintg6bZT4v4WK/kakaolink40_original.png',
+        link: {
+          mobileWebUrl: `http://localhost:3000/detail/${talentId}`,
+          webUrl: `http://localhost:3000/detail/${talentId}`,
+        },
+      },
+      social: {
+        likeCount: 286,
+        commentCount: 45,
+        sharedCount: 845,
+      },
+      buttons: [
+        {
+          title: '웹으로 보기',
+          link: {
+            mobileWebUrl: `http://localhost:3000/detail/${talentId}`,
+            webUrl: `http://localhost:3000/detail/${talentId}`,
+          },
+        },
+      ],
+    });
+  }, [detailData]);
+
   const handleClick = (): void => {
     setIsClicked(true);
-  }
+  };
 
   const changeInput = (key: string) => (event: any) => {
     setEditDetail({
       ...editDetail,
-      [key]: event.target.value 
-      });
-      input.current = event.target.value;
-  }
-
+      [key]: event.target.value,
+    });
+    input.current = event.target.value;
+  };
 
   const submitEdit = (): void => {
-    if(!editDetail.title || !editDetail.description) {
-      alert('모든 내용을 입력해주세요!')
+    if (!editDetail.title || !editDetail.description) {
+      alert('모든 내용을 입력해주세요!');
     } else {
       // body 넣을 예정
-      server.post('/talents/edit')
-      .then((response) => {
-        setDetailData(response.data);
-        setIsClicked(false);
-      })
-      .catch(()=> '');
+      server
+        .post('/talents/edit')
+        .then((response) => {
+          setDetailData(response.data);
+          setIsClicked(false);
+        })
+        .catch(() => '');
     }
-  }
+  };
 
   return (
     <CONTAINER>
@@ -72,29 +112,61 @@ function TalentDetailPage(): JSX.Element {
         <button type="button">채팅으로 거래하기</button>
       </SELLER>
       <DETAIL>
-      {isClicked ? 
-         <> 
-          <div>카테고리 : 
-            <select onBlur={event => setEditDetail(event.target.value)}>
-              {categoryList.map((category) => (
-                <option key={category}>{category}</option>
-              ))}
-            </select>
-          </div> 
-          가격 : <input type="number" ref={input} value={editDetail?.price} onChange={changeInput('price')} placeholder="입력값이 없으면 무료재능기부가 됩니다!"/>원
-          <input ref={input} value={editDetail?.title} onChange={changeInput('title')} placeholder="제목을 입력해주세요."/> 
-          <input ref={input} value={editDetail?.description} onChange={changeInput('description')} placeholder="내용을 입력해주세요."/> 
-          <button type="button" onClick={submitEdit}> Complete </button> 
-         </>
-         : 
-         <>
-          <button type="button" onClick={handleClick}> Edit </button>
-          <div>카테고리 : {editDetail?.category}</div>
-          <div>가격 : {editDetail?.price}원</div>
-          <div>{editDetail?.title}</div>
-          <div>{editDetail?.description}</div>
-         </>
-        }
+        {isClicked ? (
+          <>
+            <div>
+              카테고리 :
+              <select onBlur={(event) => setEditDetail(event.target.value)}>
+                {categoryList.map((category) => (
+                  <option key={category}>{category}</option>
+                ))}
+              </select>
+            </div>
+            가격 :{' '}
+            <input
+              type="number"
+              ref={input}
+              value={editDetail?.price}
+              onChange={changeInput('price')}
+              placeholder="입력값이 없으면 무료재능기부가 됩니다!"
+            />
+            원
+            <input
+              ref={input}
+              value={editDetail?.title}
+              onChange={changeInput('title')}
+              placeholder="제목을 입력해주세요."
+            />
+            <input
+              ref={input}
+              value={editDetail?.description}
+              onChange={changeInput('description')}
+              placeholder="내용을 입력해주세요."
+            />
+            <button type="button" onClick={submitEdit}>
+              {' '}
+              Complete{' '}
+            </button>
+          </>
+        ) : (
+          <>
+            <button type="button" onClick={handleClick}>
+              {' '}
+              Edit{' '}
+            </button>
+            <div>카테고리 : {editDetail?.category}</div>
+            <div>가격 : {editDetail?.price}원</div>
+            <div>{editDetail?.title}</div>
+            <div>{editDetail?.description}</div>
+            <button
+              style={{ border: 'none', backgroundColor: '#FEE500', padding: '5px 10px', borderRadius: '20px' }}
+              type="button"
+              className="create-kakao-link-btn"
+            >
+              카톡으로 공유하기
+            </button>
+          </>
+        )}
       </DETAIL>
       <PHOTOS>photos</PHOTOS>
       <Review />
