dc44f5c59eb267d6bb3aa18ecd085e29d4dfccbd
YU-WON <shallwedance0419@gmail.com>
2021-06-02 19:46:11 +0900

    [JAWS-95][Add] selling, purchaese list

--- src/components/pages/MyPage/Sections/PurchaseList.tsx
+++ src/components/pages/MyPage/Sections/PurchaseList.tsx
@@ -1,6 +1,38 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { PURCHASELIST } from './PurchaseListStyle';
 
-export default function PurchaseList(): JSX.Element {
-  return <PURCHASELIST>PurchaseList</PURCHASELIST>;
+import { Type } from '../MyPage';
+
+export default function PurchaseList({ reviewed, unreviewed }: any): JSX.Element {
+  return (
+    <>
+      <PURCHASELIST>
+        PurchaseList
+        <div>
+          {' '}
+          {unreviewed.map((list: Type) => (
+            <div key={Math.random()}>
+              <div>{list.title}</div>
+              <div>{list.address}</div>
+              <div>{list.category}</div>
+              <div>{list.price}</div>
+              <Link to={`/detail/${list._id}`}>리뷰쓰러가기</Link>
+            </div>
+          ))}
+        </div>
+        <div>
+          {reviewed.map((list: Type) => (
+            <div key={Math.random()}>
+              <div>{list.title}</div>
+              <div>{list.address}</div>
+              <div>{list.category}</div>
+              <div>{list.price}</div>
+              <div>내 별점: {list?.reviews}</div>
+            </div>
+          ))}
+        </div>
+      </PURCHASELIST>
+    </>
+  );
 }
