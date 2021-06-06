251446aa20e54506ab756fc322c438cc86edd098
SoonKi Kwon <jhk327@gmail.com>
2021-06-07 04:28:50 +0900

    [JAWS-112][Add] detailPage Loading spinner

--- src/utils/validateNickname.ts
+++ src/utils/validateNickname.ts
@@ -1,5 +1,5 @@
 export default function validateNickname(nickname: string): string {
-  const hasString = /^[가-힣a-z]+$/.test(nickname);
+  const hasString = /^[가-힣a-z0-9]+$/.test(nickname);
   const hasSpecialPattern = /[`~!@#$%^&*|\\\'\";:\/?]/gi.test(nickname);
   const hasSpace = nickname.search(/\s/) !== -1;
 
@@ -10,7 +10,7 @@ export default function validateNickname(nickname: string): string {
     return '닉네임에 공백이 포함되어 있습니다.';
   }
   if (!hasString) {
-    return '닉네임은 한글,영문 소문자 2~8자리로 작성해주세요.';
+    return '닉네임은 한글,영문 소문자,숫자 2~8자리로 작성해주세요.';
   }
 
   return '통과';
